e37c3145b747b7ce8fba879a0d06ec290a456810
Ankit Yadav <ay993659@gmail.com>
2026-03-19 18:41:21 +0530

    Initial commit

---
+++ Project-X-Quizerra-main/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!D:\ankit\Project-X-Quizerra-main\Project-X-Quizerra-main\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
